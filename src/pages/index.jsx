fe804b3622b5bf9bcf07ba83b92726ca337ec1d5
uslatner <ursa.slatner@gmail.com>
2024-10-07 15:02:29 +1100

    test dynamic routes

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -9,6 +9,16 @@ export default function Home() {
     <div>
       <HeroSection />
       <Loaders />
+      <h1>Spinners</h1>
+      <ul>
+        <li>
+          <Link href="/spinners/circularspinner">Circular Spinner</Link>
+        </li>
+        <li>
+          <Link href="/spinners/dotspinner">Dot Spinner</Link>
+        </li>
+        {/* Add more links as needed */}
+      </ul>
     </div>
   );
 }
