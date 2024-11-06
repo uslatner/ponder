cf278cef7877d9c47f4ac5d8f1e360601ab532b7
uslatner <ursa.slatner@gmail.com>
2024-11-06 14:29:29 +1100

    edit about pages

--- src/components/features/HeroSectionAboutUs.jsx
+++ src/components/features/HeroSectionAboutUs.jsx
@@ -2,69 +2,60 @@ import styles from "./HeroSectionAboutUs.module.scss";
 
 import Image from "next/image";
 import DBUGDS from "../../assets/DBUGDS.gif";
-import { useState } from "react";
 
 export default function HeroSection() {
-  const textToCopy = "About Us";
-  const [isCopied, setIsCopied] = useState(false);
-  const copyToClipboard = () => {
-    navigator.clipboard.writeText(textToCopy);
-    setIsCopied(true);
-    setTimeout(() => setIsCopied(false), 4000);
-  };
-
-  const scrollToLoaders = () => {
-    document
-      .getElementById("loadersSection")
-      .scrollIntoView({ behavior: "smooth" });
-  };
 
   return (
     <section className={styles.hero}>
-      <div className={styles.container}>
-        <div className={styles.container1}>
+        <div className={styles.container}>
           <div className={styles.heroImg}>
-            <Image src={DBUGDS} alt="" fill />
+            <Image src={DBUGDS} alt="debug logo" layout="responsive"  />
           </div>
         </div>
-      </div>
       <div className={styles.content}>
         <br></br>
         <h1>About Us</h1>
         <p className={styles.description}>
-          We are a team of new developers who are just starting out as new
-          developers.We have created this website to showcase our range of
-          Spinners for you, and we hope you enjoy using them and adding them to
-          your websites or apps just as much as we did creating them our team
-          members are Cate , Maria , DeeDee, Thao, Ursa and we like to give a
-          huge thank you to Alex Bicknell & Amberle Seidl for the inspiration
-          and advice and all the the help they provided to us.
+        We are a team of new developers just starting out. We created this website to showcase our range of spinners, and we hope you enjoy using them and adding them to your websites or apps as much as we enjoyed creating them. Our team members are Cate, Maria, DeeDee, Thao, and Urša (go check out our githubs!). We would also like to give a huge thank you to Alex Bicknell and Amberle Seidl for their inspiration, advice, and all the help they provided to us.
         </p>
-        <br></br>
-        <h1>Our Members:</h1>
-        <br></br>
-        <div className={styles.copyContainer}>
-          <h2>Our Member: Cate </h2>
+
+      <div className={styles.githubAccounts}>
+        <div>
+          <h2>Our Team:</h2>
+          <br />
+            <a href="https://github.com/CatePobploy" target="_blank" rel="noopener noreferrer">
+              <p className={styles.members}>Cate</p>
+            </a>
+            <a href="https://github.com/dedeepnc" target="_blank" rel="noopener noreferrer">
+              <p className={styles.members}>Dedee</p>
+            </a>
+            <a href="https://github.com/MariaMats666" target="_blank" rel="noopener noreferrer">
+              <p className={styles.members}>Maria</p>
+            </a>
+            <a href="https://github.com/ngthaoph" target="_blank" rel="noopener noreferrer">
+              <p className={styles.members}>Thao</p>
+            </a>
+            <a href="https://github.com/uslatner" target="_blank" rel="noopener noreferrer">
+              <p className={styles.members}>Ursa</p>
+            </a>
         </div>
-        <div className={styles.copyContainer}>
-          <h2>Our Member: Maria</h2>
-        </div>
-        <div className={styles.copyContainer}>
-          <h2>Our Member: DeeDee</h2>
-        </div>
-        <div className={styles.buttonWrapper}>
-          <a href="/AboutUs2">
-            <button
-              className={styles.getStartedBtn}
-              onClick={() => scrollToLoaders()}
-            >
-              Next
-            </button>
-          </a>
-          <br></br>
-          <br></br>
+
+        <div>
+          <h2>Our Incredible Mentors:</h2>
+          <br />
+            <a href="https://github.com/kuuzon" target="_blank" rel="noopener noreferrer">
+              <p className={styles.members}>Alex</p>
+            </a>
+            <a href="https://github.com/Amberside" target="_blank" rel="noopener noreferrer">
+              <p className={styles.members}>Amberle</p>
+            </a>
+            <a href="https://github.com/DanielFitzsimmons" target="_blank" rel="noopener noreferrer">
+              <p className={styles.members}>Daniel</p>
+            </a>
         </div>
       </div>
+
+        </div>
     </section>
   );
 }
